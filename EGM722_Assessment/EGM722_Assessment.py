b2f47fff2bf79e7635ff08065f0657932e087af3
littlebluestarfish <boyle-s56@ulster.ac.uk>
2024-04-08 21:08:04 +0100

    Update EGM722_Assessment.py
    Commit to main

--- EGM722_Assessment/EGM722_Assessment.py
+++ EGM722_Assessment/EGM722_Assessment.py
@@ -2,20 +2,83 @@
 import matplotlib.pyplot as plt
 import numpy
 import rasterio
+import math
 
-
-def download('C:\Users\Mervyn Boyle\Documents\GitHub\EGM722_Assessment\EGM722_Assessment\NIR_Sheet_162\162_16_2022a_4BAND.tif'):
-    pass
-
-
-download ('C:\Users\Mervyn Boyle\Documents\GitHub\EGM722_Assessment\EGM722_Assessment\NIR_Sheet_162\162_16_2022a_4BAND.tif')
 
 # Extracting the data from the red and near-infrared bands
+# insp('C:/Users/Mervyn Boyle/Documents/GitHub/EGM722_Assessment/NIR_Sheet_162/62_16_2022a_4BAND.tif')
+# filename16 = '162_16_2022a_4BAND.tif'
 
-filename16 = '162-16_2022a_4BAND.tif'
-with rasterio.open('filename16') as src:
+# importing the image
+image_file = 'C:/Users/Mervyn Boyle/Documents/GitHub/EGM722_Assessment/NIR_Sheet_162/162_16_2022a_4BAND.tif'
+sat_data = rasterio.open(image_file)
+
+
+# Calculating the dimensions of the image on earth in metres
+width_in_projected_units = sat_data.bounds.right - sat_data.bounds.left
+height_in_projected_units = sat_data.bounds.top - sat_data.bounds.bottom
+
+print("Width: {}, Height: {}".format(width_in_projected_units, height_in_projected_units))
+print("Rows: {}, Columns: {}".format(sat_data.height, sat_data.width))
+
+# Converting the pixel co-ordinates to longitudes and latitudes
+# Upper left pixel
+row_min = 0
+col_min = 0
+
+# Lower right pixel.  Rows and columns are zero indexing.
+row_max = sat_data.height - 1
+col_max = sat_data.width - 1
+
+# Transform coordinates with the dataset's affine transformation.
+topleft = sat_data.transform * (row_min, col_min)
+botright = sat_data.transform * (row_max, col_max)
+
+print("Top left corner coordinates: {}".format(topleft))
+print("Bottom right corner coordinates: {}".format(botright))
+
+# Bands
+print(sat_data.count)
+
+# sequence of band indexes
+print(sat_data.indexes)
+
+# Visualising the Satellite Imagery
+# Load the 4 bands into 2d arrays - recall that we previously learned PlanetScope band order is BGRN.
+b, g, r, n = sat_data.read()
+# Displaying the blue band.
+
+fig = plt.imshow(b)
+plt.show()
+
+# Displaying the green band.
+
+fig = plt.imshow(g)
+fig.set_cmap('gist_earth')
+plt.show()
+
+# Displaying the red band.
+
+fig = plt.imshow(r)
+fig.set_cmap('inferno')
+plt.colorbar()
+plt.show()
+
+# Displaying the infrared band.
+
+fig = plt.imshow(n)
+fig.set_cmap('winter')
+plt.colorbar()
+plt.show()
+
+
+
+
+
+
+with rasterio.open(image_file) as src:
     band_red = src.read(3)
-with rasterio.open('filename16') as src:
+with rasterio.open(image_file) as src:
     band_nir = src.read(4)
 
 # Calculating NDVI
@@ -53,9 +116,9 @@ with rasterio.open('ndvi.tif', 'w', **kwargs) as dst:
     dst.write(ndvi, 1)
 
 # Applying a color scheme to visualize the NDVI values on the new image
-from matplotlib import colours
+from matplotlib import colors
 
-class MidpointNormalise(colours.Normalise):
+class MidpointNormalize(colors.Normalize):
 
     def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
         self.midpoint = midpoint
@@ -101,7 +164,7 @@ plt.show()
 
 # Generating a histogram of NDVI values
 # Define a new figure
-fig2 = plt.figure(figsize=(20,10))
+fig2 = plt.figure(figsize=(20, 10))
 
 # Give this new figure a subplot, which will contain the histogram itself
 ax = fig2.add_subplot(111)
@@ -116,9 +179,9 @@ x = ndvi[~numpy.isnan(ndvi)]
 color = 'g'
 
 # call 'hist` with our x-axis, bins, and color details
-ax.hist(x,bins=30,color=color,histtype='bar', ec='black')
+ax.hist(x, bins=30, color=color, histtype='bar', ec='black')
 
 # Save the generated figure to an external image file
-#fig2.savefig("ndvi-histogram.png", dpi=200, bbox_inches='tight', pad_inches=0.5)
+fig2.savefig("ndvi-histogram.png", dpi=200, bbox_inches='tight', pad_inches=0.5)
 
 plt.show()
